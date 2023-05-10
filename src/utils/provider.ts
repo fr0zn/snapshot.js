f363f9b1d91a7cdffcc14b06d3184939fd9aaa1c
fr0zn <ferran.celades@hotmail.com>
2023-05-10 21:50:59 +0200

    If single RPC just return that

--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -3,11 +3,17 @@ import {
   StaticJsonRpcProvider
 } from '@ethersproject/providers';
 
+import networks from '../networks.json';
+
 const providers = {};
 const batchedProviders = {};
 
+
 export default function getProvider(network) {
-  const url = `https://rpc.snapshot.org/${network}`;
+  let url = `https://rpc.snapshot.org/${network}`;
+  if(networks[network].rpc.length == 1){
+    url = networks[network].rpc[0];
+  }
   if (!providers[network])
     providers[network] = new StaticJsonRpcProvider(
       {
@@ -21,7 +27,10 @@ export default function getProvider(network) {
 }
 
 export function getBatchedProvider(network) {
-  const url = `https://rpc.snapshot.org/${network}`;
+  let url = `https://rpc.snapshot.org/${network}`;
+  if(networks[network].rpc.length == 1){
+    url = networks[network].rpc[0];
+  }
   if (!batchedProviders[network])
     batchedProviders[network] = new JsonRpcBatchProvider({
       url,
